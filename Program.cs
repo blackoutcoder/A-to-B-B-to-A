0f3ce100c197974b3dc4f0b98795a1fa503336a6
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-12 15:42:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace A_to_B_B_to_A
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Swap swap = new Swap();
+
+            swap.ReadNumbers();
+            swap.SwapNumbers();
+            swap.Print();
+        }
+
+        public class Swap
+        {
+            int a, b;
+
+            public void Print()
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+
+            public void ReadNumbers()
+            {
+                Console.WriteLine("Enter A and press ENTER...");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter B and press ENTER...");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public void SwapNumbers()
+            {
+                int c;
+                c = a;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
+        
